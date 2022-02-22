b4040c2e0426170bc99f47c6e43466917b92b3e9
sinsarul haq vm <sinsarulhaq.pro@gmail.com>
2022-02-22 10:15:34 +0530

    header section finifhed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Header from './Header'
+import styled from "styled-components";
 
 function Home() {
   return (
-    <div>Home</div>
+      <>
+      <Header />
+      <Container> Hello </Container>
+    </>
   )
 }
 
 export default Home
+
+const Container = styled.div`
+margin-top: 40px;
+`
