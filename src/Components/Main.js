6a6741b7674ac237c7d66cb44c36e357ada08ffa
sinsarul haq vm <sinsarulhaq.pro@gmail.com>
2022-02-22 20:27:56 +0530

    complete leftside section

---
+++ src/Components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from "styled-components";
+
+function Main() {
+  return (
+    <Container>
+        Main
+    </Container>
+  )
+}
+
+export default Main
+
+const Container = styled.div`
+grid-area: main;
+`
---
+++ src/Components/RightSide.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from "styled-components";
+
+function RightSide() {
+  return (
+    <Container>
+        Right Side
+    </Container>
+  )
+}
+
+export default RightSide
+
+const Container = styled.div`
+grid-area: rightside;
+`
