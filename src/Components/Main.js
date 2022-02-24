dc55b7de71816f350a4a034bb5ed30e1f93bc118
sinsarul haq vm <sinsarulhaq.pro@gmail.com>
2022-02-24 09:27:01 +0530

    main section finished

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,16 +1,277 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
 
 function Main() {
   return (
     <Container>
-        Main
+      <ShareBox>
+        <div>
+          <img src="/images/user.svg" alt="" />
+          <button>Start a post</button>
+        </div>
+        <div>
+          <button>
+            <img src="/images/gallery.png" alt="" />
+            <span>Photo</span>
+          </button>
+          <button>
+            <img src="/images/vedio.png" alt="" />
+            <span>Video</span>
+          </button>
+          <button>
+            <img src="/images/event.png" alt="" />
+            <span>Event</span>
+          </button>
+          <button>
+            <img src="/images/article.png" alt="" />
+            <span>Article</span>
+          </button>
+        </div>
+      </ShareBox>
+      <Article>
+        <SharedActor>
+          <a>
+            <img src="/images/user.svg" alt="" />
+            <div>
+              <span>Title</span>
+              <span>Info</span>
+              <span>Date</span>
+            </div>
+          </a>
+          <button>
+            <img src="/images/dots.png" alt="" />
+          </button>
+        </SharedActor>
+        <Description>
+          Description
+        </Description>
+        <SharedImg>
+          <a>
+            <img src="/images/sharedimg.jpg" alt="" />
+          </a>
+        </SharedImg>
+        <SocialCounts>
+          <li>
+            <button>
+              <img src="/images/like2.svg" alt="" />
+              <img src="/images/clapping2.svg" alt="" />
+              <span>75</span>
+            </button>
+          </li>
+          <li>
+            <a>2 comments</a>
+          </li>
+        </SocialCounts>
+        <SocialActions>
+        <button>
+          <img src="/images/like.png" alt="" />
+          <span>Like</span>
+        </button>
+        <button>
+          <img src="/images/comment.png" alt="" />
+          <span>Comments</span>
+        </button>
+        <button>
+          <img src="/images/share.png" alt="" />
+          <span>Share</span>
+        </button>
+        <button>
+          <img src="/images/send.png" alt="" />
+          <span>Send</span>
+        </button>
+        </SocialActions>
+      </Article>
     </Container>
-  )
+  );
 }
 
-export default Main
+export default Main;
 
 const Container = styled.div`
-grid-area: main;
+  grid-area: main;
+`;
+
+const CommonCard = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  position: relative;
+  border: none;
+  border-radius: 5px;
+`;
+const ShareBox = styled(CommonCard)`
+  display: flex;
+  flex-direction: column;
+  background: #fff;
+  color: #958b7b;
+  div {
+    button {
+      outline: none;
+      color: rgba(0, 0, 0, 0.6);
+      font-size: 14px;
+      line-height: 1.5;
+      min-height: 48px;
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+      font-weight: 600;
+    }
+    &:first-child {
+      display: flex;
+      align-items: center;
+      padding: 8px 16px 0px 16px;
+      img {
+        width: 48px;
+        border-radius: 50%;
+        margin-right: 8px;
+      }
+      button {
+        margin: 4px 0;
+        flex-grow: 1;
+        border-radius: 35px;
+        padding-left: 16px;
+        border: 1px solid rgba(0, 0, 0, 0.15);
+        background-color: #fff;
+        text-align: left;
+      }
+    }
+    &:nth-child(2) {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-around;
+      padding-bottom: 4px;
+      button {
+        img {
+          margin: 0 4px 0 -2px;
+          width: 30px;
+        }
+      }
+    }
+  }
+`;
+const Article = styled(CommonCard)`
+  padding: 0;
+  margin: 0 0 8px;
+  overflow: visible;
+  background: #fff;
+`;
+const SharedActor =styled.div`
+padding-right: 40px;
+flex-wrap: nowrap;
+padding: 12px 16px 0;
+margin-bottom: 8px;
+display: flex;
+align-items: center;
+a{
+  margin-right: 12px;
+  flex-grow: 1;
+  overflow: hidden;
+  display: flex;
+  text-decoration: none;
+  img{
+    width: 48px;
+    height: 48px;
+  }
+  & > div{
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    flex-basis: 0;
+    margin-left: 8px;
+    overflow: hidden;
+    span{
+      text-align: left;
+      &:first-child{
+        font-weight: 700;
+        font-size: 14px;
+        color: rgba(0, 0, 0, 1);
+      }
+      &:nth-child(n + 1){
+        font-size: 12px;
+        color: rgba(0, 0, 0, 0.6)
+      }
+    }
+  }
+}
+button{
+    position: absolute;
+    right: 12px;
+    top: 0;
+    background: transparent;
+    border: none;
+    outline: none;
+  }
+`
+const Description = styled.div`
+padding: 0 16px;
+overflow: hidden;
+color: rgba(0,0,0,0.9);
+font-size: 14px;
+text-align: left;
+`
+
+const SharedImg = styled.div`
+margin-top: 8px;
+width: 100%;
+display: block;
+position: relative;
+background-color: #f9fafb;
+img{
+  object-fit: contain;
+  width: 100%;
+  height: 100%;
+}
+`
+
+const SocialCounts =styled.ul`
+line-height: 1.3;
+display: flex;
+align-items: flex-end;
+overflow: auto;
+margin: 0 16px;
+padding: 8px 0;
+border-bottom: 1px solid #e9e5df;
+list-style: none;
+li{
+  margin-right: 5px;
+  font-size: 12px;
+  button{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background: transparent;
+    border: none;
+    outline: none;
+  }
+}
+`
+const SocialActions = styled.div`
+align-items: center;
+display: flex;
+justify-content: space-around;
+margin: 0;
+min-height: 40px;
+padding: 4px 8px;
+button{
+  display: inline-flex;
+  align-items: center;
+  padding: 8px;
+  color: #0a66c2;
+  background: transparent;
+  border: none;
+  outline: none;
+  border-radius: 5px;
+  &:hover{
+    background: rgba(0, 0, 0, 0.1);
+  }
+  img{
+    width: 25px;
+  }
+  @media(min-width: 768px){
+    span{
+      margin-left: 8px;
+    }
+  }
+  }
 `
